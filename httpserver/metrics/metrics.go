33c5310494eb98d697fd986b178e2897d9fa21a7
joey100 <smartheshuiping@163.com>
2022-03-13 18:44:38 +0800

    Create metrics.go

---
+++ httpserver/metrics/metrics.go
@@ -0,0 +1,52 @@
+package metrics
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+func Register() {
+	err := prometheus.Register(functionLatency)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+const (
+	MetricsNamespace = "httpserver"
+)
+
+func NewTimer() *ExecutionTimer {
+	return NewExecutionTimer(functionLatency)
+}
+
+var (
+	functionLatency = CreateExecutionTimeMetric(MetricsNamespace,
+		"Time spent.")
+)
+
+func NewExecutionTimer(histo *prometheus.HistogramVec) *ExecutionTimer {
+	now := time.Now()
+	return &ExecutionTimer{
+		histo: histo,
+		start: now,
+		last:  now,
+	}
+}
+
+func (t *ExecutionTimer) ObserveTotal() {
+	(*t.histo).WithLabelValues("total").Observe(time.Now().Sub(t.start).Seconds())
+}
+
+func CreateExecutionTimeMetric(namespace string, help string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "execution_latency_seconds",
+			Help:      help,
+			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 15),
+		}, []string{"step"},
+	)
+}
